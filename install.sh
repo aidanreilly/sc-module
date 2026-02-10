ea5d485eb1d0e9ee3cbb25736166c4bb6133b88d
Aidan Reilly <aidanreilly@users.noreply.github.com>
2026-02-10 22:26:46 +0000

    some weaks
    Signed-off-by: Aidan Reilly <aidanreilly@users.noreply.github.com>
    
    Signed-off-by: Aidan Reilly <aidanreilly@users.noreply.github.com>
    
    Signed-off-by: Aidan Reilly <aidanreilly@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 # Requires SuperCollider and a working JACK configuration already installed.
-echo "Nothing to install. Ensure SuperCollider and JACK are configured."
+echo "Ensure SuperCollider and JACK are configured."
+sudo apt-get install -y liboscpack-dev cmake
+cd ~ || exit
+rm -rf sendosc
+git clone https://github.com/yoggy/sendosc.git
+cd sendosc || exit
+cmake .
+make
+sudo make install

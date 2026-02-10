64bb923913399221f17180f612dea8f152532a56
Aidan Reilly <aidanreilly@users.noreply.github.com>
2026-02-10 22:45:01 +0000

    yup
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- install.sh
+++ install.sh
@@ -2,10 +2,9 @@
 # Requires SuperCollider and a working JACK configuration already installed.
 echo "Ensure SuperCollider and JACK are configured."
 sudo apt-get install -y liboscpack-dev cmake
-cd ~ || exit
-rm -rf sendosc
-git clone https://github.com/yoggy/sendosc.git
-cd sendosc || exit
+rm -rf /tmp/sendosc
+git clone https://github.com/yoggy/sendosc.git /tmp/sendosc
+cd /tmp/sendosc || exit
 cmake .
 make
 sudo make install

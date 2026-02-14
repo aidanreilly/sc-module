c3327b3244d19f781ae6af661ae5a52ca07bb8d6
Aidan Reilly <aidanreilly@users.noreply.github.com>
2026-02-14 09:40:50 +0000

    fix startup
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- launch.sh
+++ launch.sh
@@ -8,10 +8,12 @@ if [ -f /usr/local/pisound/scripts/common/common.sh ]; then
     . /usr/local/pisound/scripts/common/common.sh
 fi
 
-export XAUTHORITY=/home/patchbox/.Xauthority
+export XAUTHORITY=/home/patch/.Xauthority
 export DISPLAY=:0.0
+export XDG_RUNTIME_DIR=/run/user/1000
 export JACK_NO_AUDIO_RESERVATION=1
 export QT_QPA_PLATFORM="offscreen"
+export QT_WEBENGINE_DISABLE_SANDBOX=1
 
 pactl unload-module module-jackdbus-detect 2>/dev/null
 pactl unload-module module-jack-sink 2>/dev/null
